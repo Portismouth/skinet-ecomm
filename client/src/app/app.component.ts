1adf47b4b3b2514dcf5fda5474edec6e47828e0d
portismouth <ozzy.gonzalez@gmail.com>
2022-09-04 22:59:39 -0600

    end of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { IPagination } from './shared/models/pagination.model';
-import { IProduct } from './shared/models/product.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     ShopComponent
